1d05e81630574b06c8b6f164eb79b9cbb88fd80d
Marcos <markitosanches@gmail.com>
2023-05-12 10:58:51 -0400

    Seance 5

---
+++ my-blog/app/Models/BlogPost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    use HasFactory;
+
+    //protected $table = "blog";
+
+    //protected $primaryKey = "blog_id";
+
+    //protected $timestamp =  false; not using created_at updated_at
+
+    
+}
